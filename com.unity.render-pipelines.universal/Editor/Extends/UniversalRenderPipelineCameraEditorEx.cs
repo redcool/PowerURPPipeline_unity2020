3f083fc98ec1c8177ae5cfe91d8bac64833a43df
ken <redcool007@qq.com>
2022-03-10 17:57:33 +0800

    refactor fsr,gamma camera

---
+++ com.unity.render-pipelines.universal/Editor/Extends/UniversalRenderPipelineCameraEditorEx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEditor.Rendering.Universal
+{
+    partial class UniversalRenderPipelineCameraEditor
+    {
+
+        partial class StylesEx
+        {
+            public static GUIContent AMDFSR = EditorGUIUtility.TrTextContent("AMD FSR 1.0", "AMD FidelityFX Super Resolution 1.0 is a cutting edge super-optimized spatial upscaling technology that produces impressive image quality at fast framerates.");
+        }
+
+        SerializedProperty m_AdditionalCameraDataRenderAMDFSR;
+        SerializedProperty colorSpaceUsage;
+
+
+        void DrawRenderSettingsEx(CameraRenderType camType, UniversalRenderPipelineAsset rpAsset)
+        {
+            EditorGUILayout.PropertyField(colorSpaceUsage);
+        }
+
+
+        void InitEx(SerializedObject m_AdditionalCameraDataSO)
+        {
+            m_AdditionalCameraDataRenderAMDFSR = m_AdditionalCameraDataSO.FindProperty("m_AMDFSR");
+            colorSpaceUsage = m_AdditionalCameraDataSO.FindProperty("colorSpaceUsage");
+        }
+
+        private void DrawPostProcessingEx()
+        {
+            if (m_AdditionalCameraDataRenderPostProcessing.boolValue)
+                EditorGUILayout.PropertyField(m_AdditionalCameraDataRenderAMDFSR, StylesEx.AMDFSR);
+        }
+    }
+}
---
+++ com.unity.render-pipelines.universal/Runtime/Extends/PostProcessDataEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityEngine.Rendering.Universal
+{
+    public partial class PostProcessData : ScriptableObject
+    {
+        public partial class ShaderResources
+        {
+
+            [Reload("Shaders/PostProcessing/EdgeAdaptiveSpatialUpsampling.compute")]
+            public ComputeShader easuCS;
+            [Reload("Shaders/PostProcessing/RobustContrastAdaptiveSharpen.compute")]
+            public ComputeShader rcasCS;
+        }
+    }
+}
